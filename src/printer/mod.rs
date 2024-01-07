65c2a739bc951886b49242b4431ac7a0b6857757
Kirill Leonov <leonov835@yandex.ru>
2024-01-07 19:03:37 +0300

    fix print

--- src/printer/mod.rs
+++ src/printer/mod.rs
@@ -1,7 +1,14 @@
 use std::collections::HashMap;
+use color_print::cprintln;
 
 pub fn print_hash(hash: HashMap<String, u64>) {
+    cprintln!("<green><bold>RESULT</></>");
+
     for (key, value) in &hash {
-        println!("{key}: {value}");
+        if key == "total" {
+            cprintln!("<green><bold>{key}: {value}</bold></green>");
+        } else {
+            println!("{key}: {value}");
+        }
     }
 }
