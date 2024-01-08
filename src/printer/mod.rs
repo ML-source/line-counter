5720887530c83920f1e6a627a59b1696d3abb01f
Kirill Leonov <leonov835@yandex.ru>
2024-01-08 23:28:53 +0300

    test update

--- src/printer/mod.rs
+++ src/printer/mod.rs
@@ -2,7 +2,7 @@ use color_print::cprintln;
 use std::collections::HashMap;
 
 pub fn print_hash(hash: HashMap<String, u64>) {
-    cprintln!("<green><bold>RESULT</></>");
+    cprintln!("<green><bold>RESULT:</></>");
 
     for (key, value) in &hash {
         if key == "total" {
